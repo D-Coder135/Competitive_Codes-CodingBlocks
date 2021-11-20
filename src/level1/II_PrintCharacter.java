fb2d0f0cc060850063734c16bf1c8c75866d0981
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-20 19:33:13 +0530

    initialize a field string of type String

--- src/level1/II_PrintCharacter.java
+++ src/level1/II_PrintCharacter.java
@@ -6,5 +6,6 @@ public class II_PrintCharacter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a String to print its character: ");
+        String string = scanner.nextLine();
     }
 }
