990747f6f8bcdebcb8e1a3b9b1b1b068d337e616
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-20 19:23:07 +0530

    add a print statement

--- src/level1/Fact.java
+++ src/level1/Fact.java
@@ -5,5 +5,7 @@ import java.util.Scanner;
 public class Fact {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number whose factorial you want to find out: ");
+
     }
 }
