226a96361afc014435a3c5ffbdea961f2bdd903a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-20 21:25:39 +0530

    add a print statement

--- src/level2/I_ArraySwap.java
+++ src/level2/I_ArraySwap.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class I_ArraySwap {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the common size for the arrays: ");
     }
 }
