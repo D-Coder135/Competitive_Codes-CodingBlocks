9d39907613c3d34ee1fbeccf0ba12bdf520b3a9e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-20 19:26:03 +0530

    add a return statement for the method fact()

--- src/level1/Fact.java
+++ src/level1/Fact.java
@@ -7,5 +7,14 @@ public class Fact {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number whose factorial you want to find out: ");
         int number = scanner.nextInt();
+        System.out.println("Factorial of " + number + " =" + fact(number));
+    }
+
+    public static int fact(int number) {
+        if (number == 0) {
+            return 1;
+        }
+
+        return number * fact(number - 1);
     }
 }
