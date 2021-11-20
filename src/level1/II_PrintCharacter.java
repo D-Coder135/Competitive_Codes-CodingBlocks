54f8c20f14b4fb5159b00e93e5786ff6917c521b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-20 19:32:49 +0530

    add a print statement

--- src/level1/II_PrintCharacter.java
+++ src/level1/II_PrintCharacter.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class II_PrintCharacter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a String to print its character: ");
     }
 }
