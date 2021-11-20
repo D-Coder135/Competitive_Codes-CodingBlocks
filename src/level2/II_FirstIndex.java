cac29601e66bcf5c0033ca0e558f45feef614aa5
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-20 22:22:13 +0530

    add some code to initialize an array with some values

--- src/level2/II_FirstIndex.java
+++ src/level2/II_FirstIndex.java
@@ -5,5 +5,13 @@ import java.util.Scanner;
 public class II_FirstIndex {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size for the array: ");
+        int size = scanner.nextInt();
+        int[] firstArray = new int[size];
+        for (int index = 0; index < size; index++) {
+            System.out.print("Enter element for " + index + " index: ");
+            firstArray[index] = scanner.nextInt();
+        }
+
     }
 }
