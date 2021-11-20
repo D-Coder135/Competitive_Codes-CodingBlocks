5dcd6a8f8b5408acd2e024e4080681223bd48d34
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-20 21:17:43 +0530

    update the name for the method

--- src/level1/III_PrintCharacterReverse.java
+++ src/level1/III_PrintCharacterReverse.java
@@ -1,7 +1,13 @@
 package level1;
 
+import java.util.Scanner;
+
 public class III_PrintCharacterReverse {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a String to print its character: ");
+        String string = scanner.nextLine();
+        System.out.println("The characters of the string are => ");
+        printCharacterReverse(string);
     }
 }
