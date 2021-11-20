3e3410cec17d67a0df846f159785234d2aeba855
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-20 19:26:52 +0530

    add some fix up to the code. Final Commit!

--- src/level1/Fact.java
+++ src/level1/Fact.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Fact {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number whose factorial you want to find out: ");
+        System.out.print("Enter the number whose factorial you want to find out: ");
         int number = scanner.nextInt();
-        System.out.println("Factorial of " + number + " =" + fact(number));
+        System.out.println("Factorial of " + number + " = " + fact(number));
     }
 
     public static int fact(int number) {
