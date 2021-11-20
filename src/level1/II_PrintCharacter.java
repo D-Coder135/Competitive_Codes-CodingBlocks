d6236da0f228c5a217561272f8816589d1fca8a0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-20 19:32:19 +0530

    add the object for the Scanner class

--- src/level1/II_PrintCharacter.java
+++ src/level1/II_PrintCharacter.java
@@ -1,7 +1,9 @@
 package level1;
 
+import java.util.Scanner;
+
 public class II_PrintCharacter {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
     }
 }
