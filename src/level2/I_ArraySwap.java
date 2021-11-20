72253bef143de01e4e8c71946f9bfa02496ee6c8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-20 21:26:21 +0530

    initialize a field of type int name size

--- src/level2/I_ArraySwap.java
+++ src/level2/I_ArraySwap.java
@@ -6,5 +6,6 @@ public class I_ArraySwap {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the common size for the arrays: ");
+        int size = scanner.nextInt();
     }
 }
