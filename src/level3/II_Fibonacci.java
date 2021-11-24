110e2a76beca02114c1d11adad3df5cc5696749d
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-24 18:58:48 +0530

    add some fix up

--- src/level3/II_Fibonacci.java
+++ src/level3/II_Fibonacci.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class II_Fibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number to find the fibonacci: ");
+        System.out.print("Enter the number to find the fibonacci: ");
         int number = scanner.nextInt();
         System.out.println(fibonacci(number));
     }
