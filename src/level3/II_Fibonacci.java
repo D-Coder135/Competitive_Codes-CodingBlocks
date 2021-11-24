c628557e2970e0fd42c1bae1e9fc9ea16c498325
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-24 18:55:21 +0530

    add a print statement inside which call the fibonacci() method

--- src/level3/II_Fibonacci.java
+++ src/level3/II_Fibonacci.java
@@ -6,6 +6,7 @@ public class II_Fibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number to find the fibonacci: ");
-        int numbers = scanner.nextInt();
+        int number = scanner.nextInt();
+        System.out.println(fibonacci(number));
     }
 }
