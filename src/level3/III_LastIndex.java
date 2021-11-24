9e8c3a059dd1ecff88e291bb30df37c1e8ed74d5
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-24 21:57:21 +0530

    add a print statement inside which call the method lastIndex()

--- src/level3/III_LastIndex.java
+++ src/level3/III_LastIndex.java
@@ -3,5 +3,6 @@ package level3;
 public class III_LastIndex {
     public static void main(String[] args) {
         int[] array = {2, 3, 4, 5, 8, 2, 5, 4, 24, 5, 5, 7};
+        System.out.println(lastIndex(array, array.length - 1, 5));
     }
 }
