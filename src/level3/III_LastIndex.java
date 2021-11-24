3fd9e8c1a26a6148dd79dd4b16bbd1a9ffdf3e6a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-24 21:58:00 +0530

    add a method of type int name lastIndex()

--- src/level3/III_LastIndex.java
+++ src/level3/III_LastIndex.java
@@ -5,4 +5,8 @@ public class III_LastIndex {
         int[] array = {2, 3, 4, 5, 8, 2, 5, 4, 24, 5, 5, 7};
         System.out.println(lastIndex(array, array.length - 1, 5));
     }
+
+    public static int lastIndex(int[] array, int i, int item) {
+        return 0;
+    }
 }
