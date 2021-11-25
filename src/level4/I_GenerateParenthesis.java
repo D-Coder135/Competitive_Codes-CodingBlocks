b2b4ac26a464f613a8cde07d3e14a74b2dcaab09
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-25 23:13:00 +0530

    add a print statement

--- src/level4/I_GenerateParenthesis.java
+++ src/level4/I_GenerateParenthesis.java
@@ -19,5 +19,6 @@ import java.util.Scanner;
 public class I_GenerateParenthesis {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
     }
 }
