b6bae65de2bdf9a38bf5b5bb3292bd6d87776650
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-25 23:13:18 +0530

    initialize a field of type int name n

--- src/level4/I_GenerateParenthesis.java
+++ src/level4/I_GenerateParenthesis.java
@@ -20,5 +20,6 @@ public class I_GenerateParenthesis {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
     }
 }
