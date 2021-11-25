7b00ef233a76abb60b8a7ae0cb1b1d92fa47e168
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-25 23:10:10 +0530

    add the problem statement as comment inside the code

--- src/level4/I_GenerateParenthesis.java
+++ src/level4/I_GenerateParenthesis.java
@@ -1,3 +1,17 @@
+/*
+ * LeetCode Question
+ * 22. Generate Parenthesis
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * Example 1:
+ * Input: n = 3
+ * Output: ["((()))","(()())","(())()","()(())","()()()"]
+ *
+ * Example 2:
+ * Input: n = 1
+ * Output: ["()"]
+ * */
+
+
 package level4;
 
 public class I_GenerateParenthesis {
