8c218a518ceedfabeeca1c9866abd89aecac4ef0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-27 10:41:35 +0530

    initialize a field of type String name string

--- src/level4/II_SubSeq.java
+++ src/level4/II_SubSeq.java
@@ -13,6 +13,7 @@ public class II_SubSeq {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the String: ");
+        String string = scanner.nextLine();
     }
 
     public static void generateSubSequences(String ques, String ans) {
