9d5055c7751fe5a789a02af834db018119b28a8c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-27 10:35:14 +0530

    add the problem statement for the II_SubSeq.java

--- src/level4/II_SubSeq.java
+++ src/level4/II_SubSeq.java
@@ -1,3 +1,10 @@
+/*
+ * Problem Statement => Generate the subsequence of a given string.
+ * Example: String = abcd
+ *          Subsequences = a, b, c, d, ab, bc, cd, abc, bcd
+ * */
+
+
 package level4;
 
 public class II_SubSeq {
