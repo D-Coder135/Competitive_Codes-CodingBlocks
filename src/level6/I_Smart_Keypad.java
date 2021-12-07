6576ab224115cc11c99e344b907c1f4bccf241f1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-08 00:04:26 +0530

    add the problem statement as comment inside the class I_Smart_Keypad.java

--- src/level6/I_Smart_Keypad.java
+++ src/level6/I_Smart_Keypad.java
@@ -1,4 +1,12 @@
+/*
+ * You will be given a numeric string S. Print all the possible codes for S.
+ * Following vector contains the codes corresponding to the digits mapped.
+ * string table[] = { " ", ".+@$", "abc", "def", "ghi", "jkl" , "mno", "pqrs" , "tuv", "wxyz" };
+ * For example, string corresponding to 0 is " " and 1 is ".+@$"
+ * */
+
 package level6;
 
 public class I_Smart_Keypad {
+
 }
