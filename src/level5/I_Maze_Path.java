aa6be4672fb3dca73c37b612ba83c8e287d3f3d9
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-07 21:52:34 +0530

    add a print statement

--- src/level5/I_Maze_Path.java
+++ src/level5/I_Maze_Path.java
@@ -17,5 +17,6 @@ import java.util.Scanner;
 public class I_Maze_Path {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the values for row and column: ");
     }
 }
