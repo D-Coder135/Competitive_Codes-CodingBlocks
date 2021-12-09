5d6e9d1530e612b6a579112abd3993d64ceedd48
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-09 14:21:54 +0530

    add a print statement

--- src/level7/I_Possible_Combinations.java
+++ src/level7/I_Possible_Combinations.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class I_Possible_Combinations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the String: ");
     }
 }
