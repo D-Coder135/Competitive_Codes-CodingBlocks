53eb58deade4d8e3d98c0f99ca91953dfd9b4c20
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-09 14:21:31 +0530

    add the object for the Scanner class inside the main() method

--- src/level7/I_Possible_Combinations.java
+++ src/level7/I_Possible_Combinations.java
@@ -1,7 +1,9 @@
 package level7;
 
+import java.util.Scanner;
+
 public class I_Possible_Combinations {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
     }
 }
