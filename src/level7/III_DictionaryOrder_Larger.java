df21736228fe9a44427b47d3056861a13e5385e4
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-09 15:08:46 +0530

    add the main() method along with some content inside it

--- src/level7/III_DictionaryOrder_Larger.java
+++ src/level7/III_DictionaryOrder_Larger.java
@@ -8,5 +8,23 @@
 
 package level7;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class III_DictionaryOrder_Larger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        List<String> results = new ArrayList<>();
+        combinations(string, "", results);
+        Collections.sort(results);
+//        System.out.println(results);
+        for (String result : results) {
+            if (result.compareTo(string) < 0) {
+                System.out.println(result);
+            }
+        }
+    }
 }
