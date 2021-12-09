318f0224e3c5ea729a12ce0720b70e64c6586794
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-09 14:22:20 +0530

    initialize a field of type String

--- src/level7/I_Possible_Combinations.java
+++ src/level7/I_Possible_Combinations.java
@@ -6,5 +6,6 @@ public class I_Possible_Combinations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the String: ");
+        String string = scanner.nextLine();
     }
 }
