ef3377f0711968eaae2e64a170dce7d50356b605
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-09 14:36:24 +0530

    add the problem statement as comment

--- src/level7/II_DictionaryOrder_Smaller.java
+++ src/level7/II_DictionaryOrder_Smaller.java
@@ -1,3 +1,10 @@
+/*
+ * Take as input str, a string. Write a recursive function which returns all the words possible by rearranging the
+ * characters of this string which are in dictionary order smaller than the given string. The output strings must be
+ * lexicographically sorted.
+ * */
+
+
 package level7;
 
 public class II_DictionaryOrder_Smaller {
