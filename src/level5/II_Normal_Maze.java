d8cbdf3c2a5ef405ad652c52ccbe456fffa3a0b7
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-10 11:17:39 +0530

    add return statement inside the if() block body

--- src/level5/II_Normal_Maze.java
+++ src/level5/II_Normal_Maze.java
@@ -15,7 +15,7 @@ public class II_Normal_Maze {
         }
 
         if (currentRow < 0 || currentRow >= maze.length || currentCol < 0 || currentCol >= maze[0].length || maze[currentRow][currentCol] == 1) {
-
+            return;
         }
     }
 }
