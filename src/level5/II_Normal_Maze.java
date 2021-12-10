7638a5ea66f46ea2e083c1e9f98d9fa3f8668843
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-10 11:12:59 +0530

    update the type for maze[][] array from boolean to int

--- src/level5/II_Normal_Maze.java
+++ src/level5/II_Normal_Maze.java
@@ -2,11 +2,11 @@ package level5;
 
 public class II_Normal_Maze {
     public static void main(String[] args) {
-        boolean[][] maze = new boolean[3][3];
+        int[][] maze = new int[3][3];
         maze_path2(maze, 0, 0);
     }
 
-    public static void maze_path2(boolean[][] maze, int currentRow, int currentCol) {
+    public static void maze_path2(int[][] maze, int currentRow, int currentCol) {
         if (currentRow == maze.length - 1 || currentCol == maze[0].length) {
 
         }
