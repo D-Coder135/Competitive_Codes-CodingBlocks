566aa5595dd46a90a4ef5dcf5b5166e725f08fb7
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-10 14:04:50 +0530

    add the problem statement as comment inside the code

--- src/level6/III_Mazepath_D2.java
+++ src/level6/III_Mazepath_D2.java
@@ -1,3 +1,16 @@
+/*
+ * Take as input N. N is the number of rows and columns on a square board. Our player starts in top-left corner of the
+ * board and must reach bottom-right corner. In one move the player can move 1 step horizontally (right) or
+ * 1 step vertically (down) or 1 step diagonally (south-east). But the diagonal step is allowed only when the player is
+ * currently on one of the diagonals (there are two diagonals)
+ *
+ * a. Write a recursive function which returns the count of different ways the player can travel across the board.
+ *    Print the value returned.
+ *
+ * b. Write a recursive function which prints moves for all valid paths across the board (void is the return type for function).
+ *
+ * */
+
 package level6;
 
 public class III_Mazepath_D2 {
