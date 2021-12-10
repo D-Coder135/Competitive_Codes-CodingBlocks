8aafacae586456ba97f8622e1ee90b9c64bd49a3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-10 13:41:16 +0530

    add a print statement inside the if() block body

--- src/level6/II_MazePath_D.java
+++ src/level6/II_MazePath_D.java
@@ -24,7 +24,7 @@ public class II_MazePath_D {
 
     public static int maze_D(int currentRow, int currentCol, int expectedRow, int expectedCol, String ans) {
         if (currentRow == expectedRow && currentCol == expectedCol) {
-
+            System.out.println(ans);
         }
         return 0;
     }
