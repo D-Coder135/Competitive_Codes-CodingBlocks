fc531cbbb8ab19fd4b003e3f7505dda2824058aa
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-10 13:30:57 +0530

    add problem statement as comment inside the code

--- src/level6/II_MazePath_D.java
+++ src/level6/II_MazePath_D.java
@@ -1,3 +1,15 @@
+/*
+ * Take as input N1 and N2, both numbers. N1 and N2 is the number of rows and columns on a rectangular board.
+ * Our player starts in top-left corner of the board and must reach bottom-right corner. In one move the player can
+ * move 1 step horizontally (right) or 1 step vertically (down) or 1 step diagonally (south-east).
+ *
+ * a. Write a recursive function which returns the count of different ways the player can travel across the board.
+ *    Print the value returned.
+ *
+ * b. Write a recursive function which prints moves for all valid paths across the board (void is the return type for function).
+ * */
+
+
 package level6;
 
 public class II_MazePath_D {
