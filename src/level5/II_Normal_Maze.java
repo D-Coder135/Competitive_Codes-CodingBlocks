177a5f1fe374d1885e19b8c3d4c3dd6f95f65dcf
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-10 11:13:25 +0530

    update the values at a particular index inside the maze array

--- src/level5/II_Normal_Maze.java
+++ src/level5/II_Normal_Maze.java
@@ -8,7 +8,7 @@ public class II_Normal_Maze {
 
     public static void maze_path2(int[][] maze, int currentRow, int currentCol) {
         if (currentRow == maze.length - 1 || currentCol == maze[0].length) {
-
+            maze[currentRow][currentCol] = 1;
         }
     }
 }
