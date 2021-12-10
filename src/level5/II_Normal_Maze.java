5c9491290ed24a5b66553a64e49d26dc101cd8a8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-10 11:10:21 +0530

    add the public method of type void name maze_path2()

--- src/level5/II_Normal_Maze.java
+++ src/level5/II_Normal_Maze.java
@@ -5,4 +5,8 @@ public class II_Normal_Maze {
         boolean[][] maze = new boolean[3][3];
         maze_path2(maze, 0, 0);
     }
+
+    public static void maze_path2(boolean[][] maze, int currentRow, int currentCol) {
+
+    }
 }
