eeb531fc291f7413eb58a51c3318110e6e9b841f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-12-10 13:46:43 +0530

    return the count field

--- src/level6/II_MazePath_D.java
+++ src/level6/II_MazePath_D.java
@@ -33,7 +33,7 @@ public class II_MazePath_D {
             count += maze_D(currentRow, currentCol + 1, expectedRow, expectedCol, ans + 'H');
             count += maze_D(currentRow + 1, currentCol, expectedRow, expectedCol, ans + 'V');
             count += maze_D(currentRow + 1, currentCol + 1, expectedRow, expectedCol, ans + 'D');
+            return count;
         }
-        return 0;
     }
 }
