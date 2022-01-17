3d567f527f580e16997ceb5da76ce0ce0173caf7
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-17 23:50:34 +0530

    add the problem statement as comment inside the code

--- src/level7/IV_NQueens.java
+++ src/level7/IV_NQueens.java
@@ -5,6 +5,12 @@
  *   File: IV_NQueens
  */
 
+/**
+ * CODING BLOCKS
+ * You are given an empty chess board of size N*N. Find the number of ways to place N queens on the board,
+ * such that no two queens can kill each other in one move. A queen can move vertically, horizontally and diagonally.
+ */
+
 package level7;
 
 public class IV_NQueens {
