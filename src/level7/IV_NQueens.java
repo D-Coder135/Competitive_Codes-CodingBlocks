0092136f09df64c79d0cbb4fa499a8788020e783
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-17 23:49:10 +0530

    add a new class IV_NQueens.java inside the package level7

---
+++ src/level7/IV_NQueens.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 17-01-2022/01/2022
+ *   Time: 11:48 PM
+ *   File: IV_NQueens
+ */
+
+package level7;
+
+public class IV_NQueens {
+}
