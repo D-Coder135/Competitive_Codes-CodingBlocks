9838bbc41e770a1e6a9c71c3f3e52edbd5e35018
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-17 23:54:56 +0530

    initialize an object of type int

--- src/level7/IV_NQueens.java
+++ src/level7/IV_NQueens.java
@@ -5,7 +5,7 @@
  *   File: IV_NQueens
  */
 
-/**
+/*
  * CODING BLOCKS
  * You are given an empty chess board of size N*N. Find the number of ways to place N queens on the board,
  * such that no two queens can kill each other in one move. A queen can move vertically, horizontally and diagonally.
@@ -18,5 +18,6 @@ import java.util.Scanner;
 public class IV_NQueens {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
     }
 }
