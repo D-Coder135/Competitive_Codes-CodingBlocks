c34c261f67755aef6ec335c914a40e4f7a7fceb5
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-18 00:25:04 +0530

    initialize a field of type int

--- src/level7/V_BoxPermutation.java
+++ src/level7/V_BoxPermutation.java
@@ -19,5 +19,6 @@ import java.util.Scanner;
 public class V_BoxPermutation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int totalBoxes = scanner.nextInt();
     }
 }
