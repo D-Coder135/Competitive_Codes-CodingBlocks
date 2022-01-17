2b14d62d8187afd455ea0384cc277adc344d7f36
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-18 00:21:36 +0530

    add a new class V_BoxPermutation.java

---
+++ src/level7/V_BoxPermutation.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 18-01-2022/01/2022
+ *   Time: 12:21 AM
+ *   File: V_BoxPermutation
+ */
+
+package level7;
+
+public class V_BoxPermutation {
+}
