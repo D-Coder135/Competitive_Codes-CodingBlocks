e8aea25894400138f2e7e503eb05a7dfbb0dfadf
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-18 22:29:58 +0530

    add some fix up to the code

--- src/level7/IV_NQueens.java
+++ src/level7/IV_NQueens.java
@@ -49,7 +49,7 @@ public class IV_NQueens {
                 return false;
             }
         }
-        for (int row = i - 1, col = j - 1; row >= 0 && col >= 0; row--, col--) {
+        for (int row = i - 1, col = j + 1; row >= 0 && col >= 0; row--, col--) {
             if (board[row][col] == 1) {
                 return false;
             }
