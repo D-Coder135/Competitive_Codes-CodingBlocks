1ea49fed3cb0f850cec3ffadddd5e8c77bb84052
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-21 10:18:52 +0530

    add the problem statement as comment inside the code

--- src/level9_DP/I_ClimbingStairs.java
+++ src/level9_DP/I_ClimbingStairs.java
@@ -5,6 +5,14 @@
  *   File: I_ClimbingStairs
  */
 
+/**
+ * Reaching to the top of a staircase, it takes n steps.
+ * The task can be accomplished either by climbing 1 step or 2 steps at a time.
+ * In how many ways can you climb to the top.
+ * Note: n will be a positive integer.
+ */
+
+
 package level9_DP;
 
 public class I_ClimbingStairs {
