5a043587894a3c1209de92fcbf644bf941233aa1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-23 00:52:48 +0530

    add the problem statement as comment inside the code

--- src/level10_DP/III_01Knapsack.java
+++ src/level10_DP/III_01Knapsack.java
@@ -5,6 +5,24 @@
  *   File: III_01Knapsack
  */
 
+/**
+ * CODING BLOCKS PROBLEM =>
+ * <p>
+ * You are packing for a vacation on the sea side and you are going to carry only one bag with
+ * capacity S (1 <= S <= 1000). You also have N (1<= N <= 1000) items that you might want to take with you to the
+ * sea-side. Unfortunately you can not fit all of them in the knapsack, so you will have to choose.
+ * For each item you are given its size and its value. You want to maximize the total value of all the items
+ * you are going to bring. What is this maximum total value?
+ * <p>
+ * Sample Input
+ * 5 4
+ * 1 2 3 2 2
+ * 8 4 0 5 3
+ * <p>
+ * Sample Output
+ * 13
+ */
+
 package level10_DP;
 
 public class III_01Knapsack {
