85f17265ca09eda1613e2899a59adf0047f8171a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-22 19:02:42 +0530

    add the problem statement as comment inside the code

--- src/level9_DP/III_MinCostClimbingStairs.java
+++ src/level9_DP/III_MinCostClimbingStairs.java
@@ -5,6 +5,26 @@
  *   File: II_MinCostClimbingStairs
  */
 
+/**
+ * CODING BLOCKS & LEETCODE QUESTION =>
+ * <p>
+ * You are given an integer array cost where cost[i] is the cost of ith step on a staircase.
+ * Once you pay the cost, you can either climb one or two steps.
+ * You can either start from the step with index 0, or the step with index 1.
+ * <p>
+ * Return the minimum cost to reach the top of the floor.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: cost = [10,15,20]
+ * Output: 15
+ * Explanation: You will start at index 1.
+ * - Pay 15 and climb two steps to reach the top.
+ * The total cost is 15.
+ */
+
+
 package level9_DP;
 
 public class III_MinCostClimbingStairs {
