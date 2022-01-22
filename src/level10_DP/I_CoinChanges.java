fb509d5611fa3827d55356a5b24ea203c638b12e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-22 14:41:58 +0530

    add a return statement

--- src/level10_DP/I_CoinChanges.java
+++ src/level10_DP/I_CoinChanges.java
@@ -31,7 +31,7 @@ public class I_CoinChanges {
 
     private static int numberOfWays(int[] coin, int amount, int i) {
         if (amount == 0) {
-            
+            return 1;
         }
         return 0;
     }
