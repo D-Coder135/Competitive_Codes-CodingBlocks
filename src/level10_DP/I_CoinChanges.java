fb19523d269d5a7046fbb29d9fc6288b91e001ce
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-22 14:32:55 +0530

    add the problem statement as comment inside the code

--- src/level10_DP/I_CoinChanges.java
+++ src/level10_DP/I_CoinChanges.java
@@ -5,6 +5,21 @@
  *   File: I_CoinChanges
  */
 
+/**
+ * Coding Blocks Problem =>
+ * <p>
+ * Given a value N, if we want to make change for N cents, and we have infinite supply of each
+ * of S = { S1, S2, .. , Sm} valued coins, In how many ways can we make the change? The order of coins does not matter.
+ * <p>
+ * <p>
+ * Sample Input
+ * 10 4
+ * 2 5 3 6
+ * <p>
+ * Sample Output
+ * 5
+ */
+
 package level10_DP;
 
 public class I_CoinChanges {
