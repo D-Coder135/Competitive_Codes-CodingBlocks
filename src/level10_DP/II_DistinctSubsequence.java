abb468633f16f70cac0484e3b522d38455dae9c5
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-22 23:51:37 +0530

    add the problem statement as comment inside the code

--- src/level10_DP/II_DistinctSubsequence.java
+++ src/level10_DP/II_DistinctSubsequence.java
@@ -5,6 +5,28 @@
  *   File: II_DistinctSubsequence
  */
 
+/**
+ * LEETCODE QUESTION 115 =>
+ * <p>
+ * Given two strings s and t, return the number of distinct subsequences of s which equals t.
+ * <p>
+ * A string's subsequence is a new string formed from the original string by deleting some (can be none) of
+ * the characters without disturbing the remaining characters' relative positions.
+ * (i.e., "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+ * <p>
+ * The test cases are generated so that the answer fits on a 32-bit signed integer.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "rabbbit", t = "rabbit"
+ * Output: 3
+ * Explanation:
+ * As shown below, there are 3 ways you can generate "rabbit" from S.
+ * rabbbit
+ * rabbbit
+ * rabbbit
+ */
+
 package level10_DP;
 
 public class II_DistinctSubsequence {
