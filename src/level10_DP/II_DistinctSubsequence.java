ffc6d129b0baa0048713da236f8559d262d45157
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-23 00:06:02 +0530

    add some comment to the code

--- src/level10_DP/II_DistinctSubsequence.java
+++ src/level10_DP/II_DistinctSubsequence.java
@@ -34,19 +34,20 @@ public class II_DistinctSubsequence {
         return numberOfWays(s, t, 0, 0);
     }
 
-    private int numberOfWays(String s, String t, int m, int n) {
-        if (n == t.length()) {
-            return 1;
-        }
-
-        if (m == s.length()) {
-            return 0;
-        }
-        int inc = 0, exc = 0;
-        if (s.charAt(m) == t.charAt(n)) {
-            inc += numberOfWays(s, t, m + 1, n + 1);
-        }
-        exc += numberOfWays(s, t, m + 1, n);
-        return inc + exc;
+//    Recursive Solution : TLE ERROR
+private int numberOfWays(String s, String t, int m, int n) {
+    if (n == t.length()) {
+        return 1;
     }
+
+    if (m == s.length()) {
+        return 0;
+    }
+    int inc = 0, exc = 0;
+    if (s.charAt(m) == t.charAt(n)) {
+        inc += numberOfWays(s, t, m + 1, n + 1);
+    }
+    exc += numberOfWays(s, t, m + 1, n);
+    return inc + exc;
+}
 }
