ed909ad2efcb6d5c2eee4b10a36cce123291ff1f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-23 13:00:57 +0530

    add the solution code inside the class II_LCS_3Strings.java

--- src/level11_DP/II_LCS_3Strings.java
+++ src/level11_DP/II_LCS_3Strings.java
@@ -7,5 +7,44 @@
 
 package level11_DP;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class II_LCS_3Strings {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        String str3 = sc.nextLine();
+//        System.out.println(lcs(str1, str2, 0, 0));
+        int[][][] dp = new int[str1.length()][str2.length()][str3.length()];
+        for (int[][] a : dp) {
+            for (int[] b : a) {
+                Arrays.fill(b, -1);
+            }
+        }
+        System.out.println(lcsDP(str1, str2, str3, 0, 0, 0, dp));
+    }
+
+    private static int lcsDP(String str1, String str2, String str3, int i, int j, int k, int[][][] dp) {
+
+        if (i == str1.length() || j == str2.length() || k == str3.length()) {
+            return 0;
+        }
+
+        if (dp[i][j][k] != -1) {
+            return dp[i][j][k];
+        }
+
+        int ans = 0;
+        if (str1.charAt(i) == str2.charAt(j) && str1.charAt(i) == str3.charAt(k)) {
+            ans = 1 + lcsDP(str1, str2, str3, i + 1, j + 1, k + 1, dp);
+        } else {
+            int fstr1 = lcsDP(str1, str2, str3, i + 1, j, k, dp);
+            int fstr2 = lcsDP(str1, str2, str3, i, j + 1, k, dp);
+            int fstr3 = lcsDP(str1, str2, str3, i, j, k + 1, dp);
+            ans = Math.max(fstr1, Math.max(fstr2, fstr3));
+        }
+        return dp[i][j][k] = ans;
+    }
 }
