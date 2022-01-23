80de18112c91f7912309642b5a5d9ab4f601a433
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-23 12:11:27 +0530

    add the problem statement as comment inside the code

--- src/level11_DP/I_LongestCommonSubsequence_LCS.java
+++ src/level11_DP/I_LongestCommonSubsequence_LCS.java
@@ -5,6 +5,24 @@
  *   File: I_LongestCommonSubsequence_LCS
  */
 
+/**
+ * LEETCODE & CODING BLOCKS PROBLEM =>
+ * <p>
+ * Given two strings text1 and text2, return the length of their longest common subsequence.
+ * If there is no common subsequence, return 0.
+ * <p>
+ * A subsequence of a string is a new string generated from the original string with some characters (can be none)
+ * deleted without changing the relative order of the remaining characters.
+ * <p>
+ * A common subsequence of two strings is a subsequence that is common to both strings.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: text1 = "abcde", text2 = "ace"
+ * Output: 3
+ * Explanation: The longest common subsequence is "ace" and its length is 3.
+ */
+
 package level11_DP;
 
 public class I_LongestCommonSubsequence_LCS {
