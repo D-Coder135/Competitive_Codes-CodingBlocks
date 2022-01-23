0aaa88a3caa5ac15fa4d284308ac9ee2fa408978
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-23 12:17:56 +0530

    add the object for the class Scanner

--- src/level11_DP/I_LongestCommonSubsequence_LCS.java
+++ src/level11_DP/I_LongestCommonSubsequence_LCS.java
@@ -25,8 +25,10 @@
 
 package level11_DP;
 
+import java.util.Scanner;
+
 public class I_LongestCommonSubsequence_LCS {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
