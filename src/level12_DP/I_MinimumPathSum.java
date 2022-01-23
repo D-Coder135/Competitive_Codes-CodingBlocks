7ad605d44c6a3c74cdeef76c8c1a9922794c676a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-23 22:52:44 +0530

    add an if() block along with a condition inside it

--- src/level12_DP/I_MinimumPathSum.java
+++ src/level12_DP/I_MinimumPathSum.java
@@ -39,6 +39,9 @@ public class I_MinimumPathSum {
     }
 
     private static int minimumPathSum(int[][] grid, int currentCol, int currentRow) {
+        if (currentCol >= grid[0].length || currentRow >= grid.length) {
+
+        }
         return 0;
     }
 }
