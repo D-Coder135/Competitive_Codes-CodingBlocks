f9d727446a20216f05836e541dfb11064eeffcbe
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-23 15:56:22 +0530

    add the problem statement as comment inside the code

--- src/level12_DP/I_MinimumPathSum.java
+++ src/level12_DP/I_MinimumPathSum.java
@@ -5,6 +5,21 @@
  *   File: I_MinimumPathSum
  */
 
+/**
+ * CODING BLOCKS & LEETCODE QUESTION =>
+ * <p>
+ * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right,
+ * which minimizes the sum of all numbers along its path.
+ * <p>
+ * Note: You can only move either down or right at any point in time.
+ * <p>
+ * Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+ * <p>
+ * Output: 7
+ * <p>
+ * Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+ */
+
 package level12_DP;
 
 public class I_MinimumPathSum {
