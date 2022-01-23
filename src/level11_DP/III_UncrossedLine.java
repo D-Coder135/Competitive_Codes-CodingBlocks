724e59330e1a593fa73d5bcc3ad318813cf82775
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-23 15:00:55 +0530

    add the problem statement as comment inside the code

--- src/level11_DP/III_UncrossedLine.java
+++ src/level11_DP/III_UncrossedLine.java
@@ -5,6 +5,31 @@
  *   File: III_UncrossedLine
  */
 
+/**
+ * CODING BLOCKS & LEETCODE QUESTION =>
+ * <p>
+ * You are given two integer arrays nums1 and nums2. We write the integers of nums1 and nums2
+ * (in the order they are given) on two separate horizontal lines.
+ * <p>
+ * We may draw connecting lines: a straight line connecting two numbers nums1[i] and nums2[j] such that:
+ * nums1[i] == nums2[j], and
+ * the line we draw does not intersect any other connecting (non-horizontal) line.
+ * <p>
+ * <p>
+ * Note that a connecting line cannot intersect even at the endpoints
+ * (i.e., each number can only belong to one connecting line).
+ * <p>
+ * Return the maximum number of connecting lines we can draw in this way.
+ * <p>
+ * Sample Input
+ * 6 5
+ * 1 3 7 1 7 5
+ * 1 9 2 5 1
+ * <p>
+ * Sample Output
+ * 2
+ */
+
 package level11_DP;
 
 public class III_UncrossedLine {
