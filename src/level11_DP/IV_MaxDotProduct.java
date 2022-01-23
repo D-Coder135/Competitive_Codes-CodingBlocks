3d3c8a02535ee8f432f004fa5e83bd686feae9d0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-23 15:49:33 +0530

    add the problem statement as comment inside the code

--- src/level11_DP/IV_MaxDotProduct.java
+++ src/level11_DP/IV_MaxDotProduct.java
@@ -5,6 +5,27 @@
  *   File: IV_MaxDotProduct
  */
 
+/**
+ * LEETCODE QUESTION 1458 =>
+ * <p>
+ * Given two arrays nums1 and nums2.
+ * <p>
+ * Return the maximum dot product between non-empty subsequences of nums1 and nums2 with the same length.
+ * <p>
+ * A subsequence of a array is a new array which is formed from the original array by deleting some (can be none)
+ * of the characters without disturbing the relative positions of the remaining characters.
+ * (i.e, [2,3,5] is a subsequence of [1,2,3,4,5] while [1,5,3] is not).
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums1 = [2,1,-2,5], nums2 = [3,0,-6]
+ * <p>
+ * Output: 18
+ * <p>
+ * Explanation: Take subsequence [2,-2] from nums1 and subsequence [3,-6] from nums2.
+ * Their dot product is (2*3 + (-2)*(-6)) = 18.
+ */
+
 package level11_DP;
 
 public class IV_MaxDotProduct {
