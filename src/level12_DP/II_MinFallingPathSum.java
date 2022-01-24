d1596c82539a329e51e8249e88b4189774eb28e0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-24 11:25:41 +0530

    add the problem statement as comment inside the code

--- src/level12_DP/II_MinFallingPathSum.java
+++ src/level12_DP/II_MinFallingPathSum.java
@@ -5,6 +5,20 @@
  *   File: II_MinFallingPathSum
  */
 
+/**
+ * LEETCODE QUESTION 931 =>
+ * <p>
+ * Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
+ * <p>
+ * A falling path starts at any element in the first row and chooses the element in the next row that is either
+ * directly below or diagonally left/right. Specifically, the next element from position (row, col) will be
+ * (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
+ * <p>
+ * Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+ * Output: 13
+ * Explanation: There are two falling paths with a minimum sum as shown.
+ */
+
 package level12_DP;
 
 public class II_MinFallingPathSum {
