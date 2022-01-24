ee50cca88c161383ebafc8ca71e41772fb15a97a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-24 11:35:30 +0530

    add the problem statement as comment inside the code

--- src/level12_DP/III_MinFallingPathSum_II.java
+++ src/level12_DP/III_MinFallingPathSum_II.java
@@ -5,6 +5,24 @@
  *   File: III_MinFallingPathSum_II
  */
 
+/**
+ * LEETCODE QUESTION 1289 =>
+ * <p>
+ * Given an n x n integer matrix grid, return the minimum sum of a falling path with non-zero shifts.
+ * <p>
+ * A falling path with non-zero shifts is a choice of exactly one element from each row of grid such that no two
+ * elements chosen in adjacent rows are in the same column.
+ * <p>
+ * Input: arr = [[1,2,3],[4,5,6],[7,8,9]]
+ * Output: 13
+ * Explanation:
+ * The possible falling paths are:
+ * [1,5,9], [1,5,7], [1,6,7], [1,6,8],
+ * [2,4,8], [2,4,9], [2,6,7], [2,6,8],
+ * [3,4,8], [3,4,9], [3,5,7], [3,5,9]
+ * The falling path with the smallest sum is [1,5,7], so the answer is 13.
+ */
+
 package level12_DP;
 
 public class III_MinFallingPathSum_II {
