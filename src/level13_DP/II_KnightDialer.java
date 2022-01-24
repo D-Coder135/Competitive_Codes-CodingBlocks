53af489d77a424bd5ec88689f4149f98a844c08e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-24 20:49:38 +0530

    comment out some part of the code

--- src/level13_DP/II_KnightDialer.java
+++ src/level13_DP/II_KnightDialer.java
@@ -38,7 +38,7 @@ public class II_KnightDialer {
         int ans = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 3; j++) {
-                ans += knightDialer(n - 1, i, j) % 1000000007;
+//                ans += knightDialer(n - 1, i, j) % 1000000007;
             }
         }
         System.out.println(ans);
