1f8078ae1984687dba968037c30d6971b85ce3f1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-24 18:19:28 +0530

    add a new class I_Triangle.java inside the package level13_DP

---
+++ src/level13_DP/I_Triangle.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 24-01-2022/01/2022
+ *   Time: 06:19 PM
+ *   File: I_Triangle
+ */
+
+package level13_DP;
+
+public class I_Triangle {
+}
