6903a7b2d524e3c52620a3a155a45443ec12bb03
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-24 18:48:42 +0530

    add the problem statement as comment inside the code

--- src/level13_DP/II_KnightDialer.java
+++ src/level13_DP/II_KnightDialer.java
@@ -5,6 +5,28 @@
  *   File: II_KnightDialer
  */
 
+/**
+ * LEETCODE QUESTION 931 =>
+ * The chess knight has a unique movement, it may move two squares vertically and one square horizontally,
+ * or two squares horizontally and one square vertically (with both forming the shape of an L).
+ * <p>
+ * We have a chess knight and a phone pad as shown below, the knight can only stand on a numeric cell (i.e. blue cell).
+ * <p>
+ * Given an integer n, return how many distinct phone numbers of length n we can dial.
+ * <p>
+ * You are allowed to place the knight on any numeric cell initially, and then you should perform n - 1
+ * jumps to dial a number of length n. All jumps should be valid knight jumps.
+ * <p>
+ * As the answer may be very large, return the answer modulo 109 + 7.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 1
+ * Output: 10
+ * Explanation: We need to dial a number of length 1, so placing the knight over any numeric cell of the 10 cells
+ * is sufficient.
+ */
+
 package level13_DP;
 
 public class II_KnightDialer {
