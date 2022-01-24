033dec097cec2b86e3216be98fb50edf5be23a5c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-24 21:14:25 +0530

    add the problem statement as comment inside the code

--- src/level13_DP/III_ValentineMagic.java
+++ src/level13_DP/III_ValentineMagic.java
@@ -5,6 +5,27 @@
  *   File: III_ValentineMagic
  */
 
+/**
+ * CODING BLOCKS QUESTION =>
+ * <p>
+ * It's Valentine's Day in Russia today. as we all know number of girls in Russia is more than number of
+ * boys 😜 hence boys have an extra advantage while choosing girl for the valentine evening. Each boy has certain
+ * number of chocolates and each girl has certain number of candies. Now you being the anchor of evening wants to
+ * pair all the boys with girls such that the sum of absolute difference between boy's chocolate and girl's candy
+ * in a pair is minimized. Of-course some girls will remain unpaired but that's okay as we are in Russia 😜
+ * <p>
+ * Sample Input
+ * 2 5
+ * 4 5
+ * 1 2 3 4 5
+ * <p>
+ * Sample Output
+ * 0
+ * <p>
+ * Explanation
+ * we can pair boy numbered 1 with girl numbered 4 and boy numbered 2 with girl numbered 5
+ */
+
 package level13_DP;
 
 public class III_ValentineMagic {
