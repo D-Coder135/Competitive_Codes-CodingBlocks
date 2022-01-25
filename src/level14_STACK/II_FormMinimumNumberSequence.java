96d1a2e9d075be56e13441ffe353678b94c883e1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-26 00:04:45 +0530

    add a for loop till n

--- src/level14_STACK/II_FormMinimumNumberSequence.java
+++ src/level14_STACK/II_FormMinimumNumberSequence.java
@@ -34,6 +34,8 @@ public class II_FormMinimumNumberSequence {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String[] array = new String[n];
+        for (int i = 0; i < n; i++) {
 
+        }
     }
 }
