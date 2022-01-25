ebf63f011f4e40c143f0430453491f2e39546d0d
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-26 00:03:14 +0530

    add the problem statement as comment inside the code

--- src/level14_STACK/II_FormMinimumNumberSequence.java
+++ src/level14_STACK/II_FormMinimumNumberSequence.java
@@ -5,6 +5,26 @@
  *   File: II_FormMinimumNumberSequence
  */
 
+/**
+ * CODING BLOCKS QUESTION =>
+ * <p>
+ * Given an array of patterns containing only I’s and D’s. I for increasing and D for decreasing.
+ * Devise an algorithm to print the minimum number following that pattern. Digits from 1-9 and digits can’t repeat.
+ * <p>
+ * Sample Input
+ * 4
+ * D I DD II
+ * <p>
+ * Sample Output
+ * 21
+ * 12
+ * 321
+ * 123
+ * <p>
+ * Explanation
+ * For the Given sample case, For a Pattern of 'D' print a decreasing sequence which is 2 1.
+ */
+
 package level14_STACK;
 
 public class II_FormMinimumNumberSequence {
