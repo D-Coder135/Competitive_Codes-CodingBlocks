e55a20b3307b2b0dd9e8ccc257fed7e785e9a515
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-26 00:04:08 +0530

    initialize a field of type int

--- src/level14_STACK/II_FormMinimumNumberSequence.java
+++ src/level14_STACK/II_FormMinimumNumberSequence.java
@@ -32,5 +32,6 @@ import java.util.Scanner;
 public class II_FormMinimumNumberSequence {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
     }
 }
