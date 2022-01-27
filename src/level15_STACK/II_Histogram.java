b1b1339e688cad2d47cc082683a8425d8f42c6b9
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-28 00:21:30 +0530

    add a for loop along with an if() block inside it

--- src/level15_STACK/II_Histogram.java
+++ src/level15_STACK/II_Histogram.java
@@ -36,5 +36,10 @@ public class II_Histogram {
     private static int maximumArea(int[] arr) {
         Stack<Integer> stack = new Stack<>();
         int maxArea = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (stack.isEmpty() || arr[i] > arr[stack.peek()]) {
+
+            }
+        }
     }
 }
