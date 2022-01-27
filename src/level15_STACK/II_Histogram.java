a175eb34d1ac7e2f1efc013fa51c1f63b3c5edc1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-28 00:08:55 +0530

    add the problem statement as comment inside the code

--- src/level15_STACK/II_Histogram.java
+++ src/level15_STACK/II_Histogram.java
@@ -5,6 +5,18 @@
  *   File: II_Histogram
  */
 
+/**
+ * CODING BLOCKS QUESTION & LEETCODE QUESTION 84 =>
+ * <p>
+ * Find the largest rectangular area possible in a given histogram where the largest rectangle can be made of a
+ * number of contiguous bars.
+ * <p>
+ * Input: heights = [2,1,5,6,2,3]
+ * Output: 10
+ * Explanation: The above is a histogram where width of each bar is 1.
+ * The largest rectangle is shown in the red area, which has an area = 10 units.
+ */
+
 package level15_STACK;
 
 public class II_Histogram {
