aca0b76a60e6dc6e1c004ebb9e3208360bd0dff9
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-27 22:41:57 +0530

    add a for loop

--- src/level15_STACK/I_NextGreaterElement.java
+++ src/level15_STACK/I_NextGreaterElement.java
@@ -58,5 +58,8 @@ public class I_NextGreaterElement {
     private static void nextGreaterElement(int[] arr) {
         Stack<Integer> stack = new Stack<>();
         int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+
+        }
     }
 }
