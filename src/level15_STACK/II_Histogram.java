18bd05a6a7b746680ffac728c29d7c1391821b44
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-28 00:13:17 +0530

    update the value inside the arr array object

--- src/level15_STACK/II_Histogram.java
+++ src/level15_STACK/II_Histogram.java
@@ -27,7 +27,7 @@ public class II_Histogram {
         int size = scanner.nextInt();
         int[] arr = new int[size];
         for (int i = 0; i < size; i++) {
-
+            arr[i] = scanner.nextInt();
         }
     }
 }
