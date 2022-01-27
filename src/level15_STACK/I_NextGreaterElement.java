25d9dc3857a0d4724de8fdb7dc35385d8aa2dbd8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-27 22:43:44 +0530

    add a while loop

--- src/level15_STACK/I_NextGreaterElement.java
+++ src/level15_STACK/I_NextGreaterElement.java
@@ -59,7 +59,9 @@ public class I_NextGreaterElement {
         Stack<Integer> stack = new Stack<>();
         int[] ans = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[i] > arr[stack.peek()]) {
 
+            }
         }
     }
 }
