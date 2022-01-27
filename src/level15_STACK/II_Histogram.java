fc5a09eb4dfe54f2ee3ced87d82cf97e0efefcdb
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-28 00:23:39 +0530

    call the push() method

--- src/level15_STACK/II_Histogram.java
+++ src/level15_STACK/II_Histogram.java
@@ -38,7 +38,7 @@ public class II_Histogram {
         int maxArea = 0;
         for (int i = 0; i < arr.length; i++) {
             if (stack.isEmpty() || arr[i] > arr[stack.peek()]) {
-
+                stack.push(i);
             }
         }
     }
