9accb99edf986ff3f02cdf1914baa75589ce9b2a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-27 22:59:42 +0530

    update the values inside the ans array object

--- src/level15_STACK/I_NextGreaterElement.java
+++ src/level15_STACK/I_NextGreaterElement.java
@@ -60,7 +60,7 @@ public class I_NextGreaterElement {
         int[] ans = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
             while (!stack.isEmpty() && arr[i] > arr[stack.peek()]) {
-
+                ans[stack.pop()] = arr[i];
             }
         }
     }
