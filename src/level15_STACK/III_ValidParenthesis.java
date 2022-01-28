21c2ffd5e439e4d76970138d00487c701cb2d1fc
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-28 12:38:23 +0530

    add the problem statement as comment inside the code

--- src/level15_STACK/III_ValidParenthesis.java
+++ src/level15_STACK/III_ValidParenthesis.java
@@ -5,6 +5,23 @@
  *   File: III_ValidParenthesis
  */
 
+/**
+ * CODING BLOCKS QUESTION & LEETCODE QUESTION 20 =>
+ * <p>
+ * You are given a string of brackets i.e. '{', '}' , '(' , ')', '[' , ']' . You have to check whether the sequence
+ * of parenthesis is balanced or not.
+ * For example, "(())", "(())()" are balanced and "())(", "(()))" are not.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "()"
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: s = "()[]{}"
+ * Output: true
+ */
+
 package level15_STACK;
 
 public class III_ValidParenthesis {
