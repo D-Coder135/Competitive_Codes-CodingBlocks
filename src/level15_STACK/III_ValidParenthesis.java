bbff93acebf78f18b1b86af2cbb83127724aa77a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-28 12:42:22 +0530

    initialize a field of type char

--- src/level15_STACK/III_ValidParenthesis.java
+++ src/level15_STACK/III_ValidParenthesis.java
@@ -36,7 +36,7 @@ public class III_ValidParenthesis {
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < str.length(); i++) {
-
+            char character = str.charAt(i);
         }
 
         return stack.isEmpty();
