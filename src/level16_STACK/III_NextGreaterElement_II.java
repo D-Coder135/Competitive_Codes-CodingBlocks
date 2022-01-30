261cdefd9e1e17fdfeb51d4a4bddee8293afbf88
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-30 12:03:47 +0530

    add the problem statement as comment inside the code

--- src/level16_STACK/III_NextGreaterElement_II.java
+++ src/level16_STACK/III_NextGreaterElement_II.java
@@ -5,6 +5,24 @@
  *   File: III_NextGreaterElement_II
  */
 
+/**
+ * CODING BLOCKS QUESTION & LEETCODE QUESTION 503 =>
+ * <p>
+ * Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next
+ * greater number for every element in nums.
+ * <p>
+ * The next greater number of a number x is the first greater number to its traversing-order next in the array,
+ * which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this
+ * number.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,1]
+ * Output: [2,-1,2]
+ * Explanation: The first 1's next greater number is 2;
+ * The number 2 can't find next greater number.
+ * The second 1's next greater number needs to search circularly, which is also 2.
+ */
 package level16_STACK;
 
 public class III_NextGreaterElement_II {
