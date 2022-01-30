9d0324fe563a41171f747938bf0e0bb2e8f6c2d8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-30 11:16:42 +0530

    add the problem statement as comment inside the code

--- src/level16_STACK/I_EvaluateExpression.java
+++ src/level16_STACK/I_EvaluateExpression.java
@@ -5,6 +5,23 @@
  *   File: I_EvaluateExpression
  */
 
+/**
+ * CODING BLOCKS QUESTION & LEETCODE QUESTION 150 =>
+ * <p>
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ * Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+ * <p>
+ * <p>
+ * Sample Input
+ * 2
+ * 231*+9-
+ * 123+*8-
+ * <p>
+ * Sample Output
+ * -4
+ * -3
+ */
+
 package level16_STACK;
 
 public class I_EvaluateExpression {
