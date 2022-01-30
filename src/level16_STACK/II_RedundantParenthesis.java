5752abfcabde296e4646f9d238cc55e94aae673e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-30 11:33:19 +0530

    add the problem statement as comment inside the code

--- src/level16_STACK/II_RedundantParenthesis.java
+++ src/level16_STACK/II_RedundantParenthesis.java
@@ -5,6 +5,25 @@
  *   File: II_RedundantParenthesis
  */
 
+/**
+ * CODING BLOCKS QUESTION =>
+ * <p>
+ * You are given a balanced expression. You have to find if it contains duplicate parentheses or not.
+ * A set of parentheses are duplicate if same subexpression is surrounded by multiple parenthesis.
+ * <p>
+ * Sample Input
+ * 2
+ * (((a+(b))+(c+d)))
+ * ((a+(b))+(c+d))
+ * <p>
+ * Sample Output
+ * Duplicate
+ * Not Duplicates
+ * <p>
+ * Explanation
+ * For 1st test case : The subexpression "a+(b)" is surrounded by two pairs of brackets.
+ */
+
 package level16_STACK;
 
 public class II_RedundantParenthesis {
