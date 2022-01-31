ccc37296aacb24452fbf4d5019e916ef55ef26e6
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-31 19:01:29 +0530

    add while() loop

--- src/level18_LinkedList/II_ReverseLinkedList.java
+++ src/level18_LinkedList/II_ReverseLinkedList.java
@@ -40,6 +40,10 @@ public class II_ReverseLinkedList {
         public ListNode reverseList(ListNode head) {
             ListNode prev = null;
             ListNode current = head;
+
+            while (current != null) {
+
+            }
         }
     }
 }
