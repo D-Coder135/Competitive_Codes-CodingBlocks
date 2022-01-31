e3c64621a5a120ed629bdc84d893b1bb5b903911
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-31 18:38:16 +0530

    add while loop along with a condition inside it

--- src/level18_LinkedList/I_MiddleNode.java
+++ src/level18_LinkedList/I_MiddleNode.java
@@ -47,6 +47,10 @@ public class I_MiddleNode {
         public ListNode middleNode(ListNode head) {
             ListNode slow = head;
             ListNode fast = head;
+
+            while (fast != null && fast.next != null) {
+
+            }
         }
     }
 }
