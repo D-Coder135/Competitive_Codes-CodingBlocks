977da68c6c16833b5609d725de459d1b33f5969c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-31 19:07:21 +0530

    add the problem statement as comment inside the code

--- src/level18_LinkedList/III_LinkedListCycle.java
+++ src/level18_LinkedList/III_LinkedListCycle.java
@@ -5,6 +5,24 @@
  *   File: III_LinkedListCycle
  */
 
+/**
+ * LEETCODE QUESTION 141 =>
+ * <p>
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ * <p>
+ * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously
+ * following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is
+ * connected to. Note that pos is not passed as a parameter.
+ * <p>
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+ */
+
 package level18_LinkedList;
 
 public class III_LinkedListCycle {
