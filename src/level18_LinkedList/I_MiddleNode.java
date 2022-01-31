55d24afe625d9780197ee9e440fb16a9df1f76a4
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-31 18:24:40 +0530

    add a new class I_MiddleNode.java inside the package level18_LinkedList

---
+++ src/level18_LinkedList/I_MiddleNode.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 31-01-2022/01/2022
+ *   Time: 06:24 PM
+ *   File: I_MiddleNode
+ */
+
+package level18_LinkedList;
+
+public class I_MiddleNode {
+}
