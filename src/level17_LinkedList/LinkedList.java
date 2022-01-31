7c781a0bbbee1501c6461f599de37d3907fb76e1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-31 12:15:36 +0530

    add a new class LinkedList.java inside the package level17_LinkedList

---
+++ src/level17_LinkedList/LinkedList.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 31-01-2022/01/2022
+ *   Time: 12:15 PM
+ *   File: LinkedList
+ */
+
+package level17_LinkedList;
+
+public class LinkedList {
+}
