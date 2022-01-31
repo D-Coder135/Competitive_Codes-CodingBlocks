34010debc063607531e3cae7814d8230e56f36bf
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-31 22:31:59 +0530

    add a new class IV_DeleteNode.java

---
+++ src/level18_LinkedList/IV_DeleteNode.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 31-01-2022/01/2022
+ *   Time: 10:31 PM
+ *   File: IV_DeleteNode
+ */
+
+package level18_LinkedList;
+
+public class IV_DeleteNode {
+}
