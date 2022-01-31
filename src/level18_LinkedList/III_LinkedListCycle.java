4d321c225b29ffaa56eddc533642f3eabc0ccc42
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-31 22:26:27 +0530

    add an if() block

--- src/level18_LinkedList/III_LinkedListCycle.java
+++ src/level18_LinkedList/III_LinkedListCycle.java
@@ -44,6 +44,10 @@ public class III_LinkedListCycle {
             while (fast != null && fast.next != null) {
                 slow = slow.next;
                 fast = fast.next.next;
+
+                if (slow == fast) {
+
+                }
             }
         }
     }
