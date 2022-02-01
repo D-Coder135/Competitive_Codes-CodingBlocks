5c99bd2cd77962deee755bf8e90862bfb6f7a494
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-01 19:45:48 +0530

    add another static inner class Solution along with the public method mergeTwoLists() of type ListNode

--- src/level19_LinkedList/III_MergeTwoLists.java
+++ src/level19_LinkedList/III_MergeTwoLists.java
@@ -44,4 +44,10 @@ public class III_MergeTwoLists {
             this.next = next;
         }
     }
+
+    static class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        }
+    }
 }
