879dbcc906d46e2990644461d832afa4b921ef08
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-01 19:44:21 +0530

    add the problem statement as comment inside the code

--- src/level19_LinkedList/III_MergeTwoLists.java
+++ src/level19_LinkedList/III_MergeTwoLists.java
@@ -5,6 +5,26 @@
  *   File: III_MergeTwoLists
  */
 
+/**
+ * LEETCODE QUESTION 21 =>
+ * <p>
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * <p>
+ * Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two
+ * lists.
+ * <p>
+ * Return the head of the merged linked list.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ * Example 2:
+ * <p>
+ * Input: list1 = [], list2 = []
+ * Output: []
+ */
+
 package level19_LinkedList;
 
 public class III_MergeTwoLists {
