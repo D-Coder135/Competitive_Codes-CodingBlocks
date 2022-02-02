b715f594dbc91dc4a33ca1406c899c8250fbb421
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-02 16:04:39 +0530

    initialize a field of type int

--- src/level19_LinkedList/V_AddTwoNumbers.java
+++ src/level19_LinkedList/V_AddTwoNumbers.java
@@ -44,6 +44,7 @@ public class V_AddTwoNumbers {
         public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
             ListNode temp = new ListNode();
             ListNode ans = temp;
+            int carry = 0;
         }
     }
 }
