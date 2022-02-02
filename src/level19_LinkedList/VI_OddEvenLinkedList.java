0b0862f9e70acc25fce25698e8314a3008041c08
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-02 22:54:22 +0530

    add a new class VI_OddEvenLinkedList.java

---
+++ src/level19_LinkedList/VI_OddEvenLinkedList.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 02-02-2022/02/2022
+ *   Time: 10:54 PM
+ *   File: VI_OddEvenLinkedList
+ */
+
+package level19_LinkedList;
+
+public class VI_OddEvenLinkedList {
+}
