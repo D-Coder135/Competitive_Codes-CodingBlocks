659b6d1af29d8c37e45c879c9a6b59d84279c57b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-02 16:03:08 +0530

    add a public static inner class ListNode

--- src/level19_LinkedList/V_AddTwoNumbers.java
+++ src/level19_LinkedList/V_AddTwoNumbers.java
@@ -23,4 +23,20 @@
 package level19_LinkedList;
 
 public class V_AddTwoNumbers {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
