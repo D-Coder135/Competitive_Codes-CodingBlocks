a4df71d2faa2586552efdf77e15c2ec4246c1889
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-02 23:01:02 +0530

    add a static inner class Solution along with a method oddEvenList() of type ListNode containing the solution code inside its body

--- src/level19_LinkedList/VI_OddEvenLinkedList.java
+++ src/level19_LinkedList/VI_OddEvenLinkedList.java
@@ -45,4 +45,25 @@ public class VI_OddEvenLinkedList {
             this.next = next;
         }
     }
+
+    static class Solution {
+        public ListNode oddEvenList(ListNode head) {
+
+            if (head == null) return null;
+
+            ListNode odd = head;
+            ListNode even = head.next;
+            ListNode ans = even;
+
+            while (even != null && even.next != null) {
+                odd.next = odd.next.next;
+                even.next = even.next.next;
+                odd = odd.next;
+                even = even.next;
+            }
+
+            odd.next = ans;
+            return head;
+        }
+    }
 }
