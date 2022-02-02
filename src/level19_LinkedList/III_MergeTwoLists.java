1259cbe418b7d5d23488c9f7cbde40b69c5894e8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-02 11:18:36 +0530

    add some fix up

--- src/level19_LinkedList/III_MergeTwoLists.java
+++ src/level19_LinkedList/III_MergeTwoLists.java
@@ -55,12 +55,11 @@ public class III_MergeTwoLists {
                 if (list1.val <= list2.val) {
                     temp.next = list1;
                     list1 = list1.next;
-                    temp = temp.next;
                 } else {
                     temp.next = list2;
                     list2 = list2.next;
-                    temp = temp.next;
                 }
+                temp = temp.next;
             }
 
             if (list1 != null) {
