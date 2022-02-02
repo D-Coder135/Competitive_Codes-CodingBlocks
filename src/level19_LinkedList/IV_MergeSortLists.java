47e71fa7c7cc7651731bc069c691420ad3aab700
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-02 11:24:16 +0530

    update the slow and fast fields

--- src/level19_LinkedList/IV_MergeSortLists.java
+++ src/level19_LinkedList/IV_MergeSortLists.java
@@ -46,7 +46,8 @@ public class IV_MergeSortLists {
             ListNode fast = head;
 
             while (fast.next != null && fast.next.next != null) {
-
+                slow = slow.next;
+                fast = fast.next.next;
             }
         }
     }
