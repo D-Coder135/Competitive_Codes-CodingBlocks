42ec31d0a57e7c5bdb57c7a3c1817bc9155d3830
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-07 22:22:11 +0530

    add a new class BinaryTree.java inside the package level20_binaryTree

---
+++ src/level20_binaryTree/BinaryTree.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 07-02-2022/02/2022
+ *   Time: 10:21 PM
+ *   File: BinaryTree
+ */
+
+package level20_binaryTree;
+
+public class BinaryTree {
+}
