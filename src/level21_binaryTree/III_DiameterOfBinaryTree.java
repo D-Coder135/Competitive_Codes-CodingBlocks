e5efec7b84c8d6ae57524a0e9f0e35e2d151a943
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-09 22:43:20 +0530

    add the problem statement as comment inside the code

--- src/level21_binaryTree/III_DiameterOfBinaryTree.java
+++ src/level21_binaryTree/III_DiameterOfBinaryTree.java
@@ -5,6 +5,28 @@
  *   File: III_DiameterOfBinaryTree
  */
 
+/**
+ * LEETCODE QUESTION 543 =>
+ * <p>
+ * Given the root of a binary tree, return the length of the diameter of the tree.
+ * The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may
+ * or may not pass through the root.
+ * The length of a path between two nodes is represented by the number of edges between them.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: root = [1,2,3,4,5]
+ * Output: 3
+ * <p>
+ * Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: root = [1,2]
+ * Output: 1
+ */
+
 package level21_binaryTree;
 
 public class III_DiameterOfBinaryTree {
