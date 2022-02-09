be43c8cd6df5981e1847ca9e67b65e600e86c9a1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-09 18:34:15 +0530

    add some pre-written code inside the class II_LevelOrderLineWise.java

--- src/level21_binaryTree/II_LevelOrderLineWise.java
+++ src/level21_binaryTree/II_LevelOrderLineWise.java
@@ -7,5 +7,43 @@
 
 package level21_binaryTree;
 
+import java.util.Scanner;
+
 public class II_LevelOrderLineWise {
+    private final Node root;
+    Scanner scanner = new Scanner(System.in);
+
+    public II_LevelOrderLineWise(Node root) {
+        this.root = createTree();
+    }
+
+    private Node createTree() {
+        int item = scanner.nextInt();
+        Node newNode = new Node(item);
+        boolean hasLeftChild = scanner.nextBoolean();
+
+        if (hasLeftChild) {
+            newNode.left = createTree();
+        }
+
+        boolean hasRightChild = scanner.nextBoolean();
+
+        if (hasRightChild) {
+            newNode.right = createTree();
+        }
+        return newNode;
+    }
+
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node() {
+        }
+    }
 }
