5046f494b5aef58ddf8d7ff666839a6f70ae8653
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-09 18:19:37 +0530

    add some pre-written code inside the class I_LevelOrderTraversal.java

--- src/level21_binaryTree/I_LevelOrderTraversal.java
+++ src/level21_binaryTree/I_LevelOrderTraversal.java
@@ -7,5 +7,43 @@
 
 package level21_binaryTree;
 
+import java.util.Scanner;
+
 public class I_LevelOrderTraversal {
+    private final Node root;
+    Scanner scanner = new Scanner(System.in);
+
+    public I_LevelOrderTraversal() {
+        this.root = createTree(null);
+    }
+
+    private Node createTree(Node parentNode) {
+        int item = scanner.nextInt();
+        Node newNode = new Node(item);
+        boolean hasLeftChild = scanner.nextBoolean();
+
+        if (hasLeftChild) {
+            newNode.left = createTree(newNode);
+        }
+
+        boolean hasRightChild = scanner.nextBoolean();
+
+        if (hasRightChild) {
+            newNode.right = createTree(newNode);
+        }
+        return newNode;
+    }
+
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node() {
+        }
+    }
 }
