9a74f3fe025637c89970a9ae1ff21d31f3521882
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-15 22:40:55 +0530

    add the problem statement as comment inside the code

--- src/level7/IV_Permutations.java
+++ src/level7/IV_Permutations.java
@@ -5,6 +5,23 @@
  *   File: IV_Permutations
  */
 
+/**
+ * LEETCODE QUESTION 46 =>
+ * <p>
+ * Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any
+ * order.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,3]
+ * Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [0,1]
+ * Output: [[0,1],[1,0]]
+ */
+
 package level7;
 
 public class IV_Permutations {
